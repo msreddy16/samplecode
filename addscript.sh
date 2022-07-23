f0a3581e5924be999f09debafc85d6d29617e68a
msreddy16 <61373844+msreddy16@users.noreply.github.com>
2022-07-23 20:14:42 +0530

    added third variable

--- addscript.sh
+++ addscript.sh
@@ -2,6 +2,7 @@
 echo "This script is from github"
 NUMBERONE=$1
 NUMBERTWO=$2
-NUMBER=$(($NUMBERONE + $NUMBERTWO))
+NUMBERTHREE=$3
+NUMBER=$(($NUMBERONE + $NUMBERTWO + $NUMBERTHREE))
 echo "Sum is: $NUMBER"
 echo "This is completed"
